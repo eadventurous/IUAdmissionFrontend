f18894659300ad15ae1d5701d7a5d9d2361ac25a
eadventurous <e.o.ivkov@gmail.com>
2019-03-09 14:49:12 +0300

    sign in page added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
 import Admin from "layouts/Admin.jsx";
+import SignIn from "layouts/SignIn.jsx";
 
 import "assets/css/material-dashboard-react.css?v=1.6.0";
 
@@ -13,6 +14,7 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
+      <Route path="/login" component={SignIn} />
       <Route path="/admin" component={Admin} />
       <Redirect from="/" to="/admin/dashboard" />
     </Switch>
